9057f3cae3aba59ebb20bb4556c52f0e3637f36a
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-20 02:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from linkedin_api import Linkedin
 
 app = Flask(__name__)
 
@@ -6,8 +7,14 @@ app = Flask(__name__)
 @app.route('/info')
 def hello():
     
+    perfil = 'micaela-patricia-jasarevic-10a883140'
+
+    api = Linkedin('guillermoguzman.2016@gmail.com', 'guille16')
+
+    profile = api.get_profile(perfil)
+    
     data = {
-        'summary': 'prueba',
+        'summary': profile['firstName'],
         'raw': 'Successful',
     }
 
