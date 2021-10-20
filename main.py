ff4034b19371d559c3627db7a672e9c89e02b9dd
guilleguzman2019 <guillermoguzman.2016@gmail.com>
2021-10-20 02:12:20 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from linkedin_api import Linkedin
+
+app = Flask(__name__)
+
+@app.route("/info")
+def hello():
+
+    perfil = 'micaela-patricia-jasarevic-10a883140'
+
+    api = Linkedin('guillermoguzman.2016@gmail.com', 'guille16')
+
+    profile = api.get_profile(perfil)
+
+    data = {
+        'summary': profile['firstName'],
+        'raw': 'Successful',
+    }
+
+    return data
+
+
+app.run(host='0.0.0.0')
+
