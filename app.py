40ff8aecc23216a2acfd46fa36d30bf17f25f592
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-23 00:37:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,28 @@ app = Flask(__name__)
 
 @app.route('/info')
 def hello():
-    
     html = """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <!-- head definitions go here -->
-                </head>
-                <body>
-                    <h1>Hola mundo!!!</h1>
-                </body>
-            </html>
-          """
-    
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <!-- head definitions go here -->
+            </head>
+            <body>
+                <h1>Hola mundo!!</h1>
+            </body>
+        </html>
+        """
+
+    from fpdf import FPDF, HTMLMixin
+
     class MyFPDF(FPDF, HTMLMixin):
-        pass
+	    pass
 
     pdf = MyFPDF()
-    #First page
     pdf.add_page()
     pdf.write_html(html)
-    pdf.output('html2pdf.pdf', 'F')
+
+    pdf.output('example.pdf', 'F')
     
     data = {
         'summary': response_dict['emailAddress'],
