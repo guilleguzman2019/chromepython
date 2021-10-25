85ada18097600d807524b6a34fba4cb70a3b087e
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-24 23:43:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from datetime import datetime
+import pydf
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    pdf = pydf.generate_pdf('<h1>this is html</h1>')
+    with open('test_doc.pdf', 'wb') as f:
+        f.write(pdf)
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
