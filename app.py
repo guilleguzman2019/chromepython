d33e013e2625e12ad82204bf9abef3e9aeb2aa41
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-24 23:56:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from datetime import datetime
-import pydf
+from fpdf import FPDF
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    pdf = pydf.generate_pdf('<h1>this is html</h1>')
-    with open('test_doc.pdf', 'wb') as f:
-        f.write(pdf)
+    
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+    pdf.output("simple_demo.pdf")
+    
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
