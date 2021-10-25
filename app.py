bd1ee16279efbdd33e75267c83b767a50a461f93
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-25 00:08:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=12)
-    pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
-    pdf.output("simple_demo.pdf")
+    f = open ('holamundo.txt','w')
+    f.write('hola mundo')
+    f.close()
     
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
