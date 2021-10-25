9d309cdbf940f758f5bcdcd00090a983ab338ad0
guilleguzman <guillermoguzman.2016@gmail.com>
2021-10-24 23:17:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask
-import pydf
 
 app = Flask(__name__)
 
 
-@app.route('/info')
+@app.route('/')
 def hello():
-    pdf = pydf.generate_pdf('<h1>this is html</h1>')
-    with open('test_doc.pdf', 'wb') as f:
-        f.write(pdf)
-    
-    data = {
-        'summary': response_dict['emailAddress'],
-        'raw': 'Successful',
-    }
-
-    return data
-   
+    return 'Hello, World!'
